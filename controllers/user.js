2b4ad1d44a468e1a02782b5e7974aa6106d511b5
Robin FOREST <robin.batardforest@viacesi.fr>
2019-07-30 15:37:16 +0200

    login logout

--- controllers/user.js
+++ controllers/user.js
@@ -37,5 +37,28 @@ module.exports = {
                 reject(error)
             })
         });
+    },
+
+    loginUser: (email, password) => {
+        return new Promise((resolve, reject) => {
+            auth.signInWithEmailAndPassword(email, password)
+            .then(data => {
+                resolve(data)
+                console.log(auth.currentUser)
+            })
+            .catch(error => {
+                reject(error)
+            })
+        })
+    },
+
+    logoutUser: () => {
+        return new Promise((resolve, reject) => {
+            auth.signOut().then(() => {
+                resolve('success logout'); // Sign-out successful.
+            }).catch(error => {
+                reject(error)
+            })
+        })
     }
 }
