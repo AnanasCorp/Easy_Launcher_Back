e0e113bdd13b596c7e9bc8c3db1d50e9a1a1d955
Robin Snow <robin.batardforest@gmail.com>
2021-08-13 17:43:40 +0200

    trying remove body parser

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 require('dotenv').config()
-const iconv = require('iconv-lite');
 const express = require("express")
 const firebase = require("firebase")
 const cors = require("cors")
 const app = express()
 const rateLimit = require("express-rate-limit");
-
-iconv.extendNodeEncodings();
 
 app.use(express.json());
 app.use(express.urlencoded({
